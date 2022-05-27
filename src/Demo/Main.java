d0ef8efae04ce49f7b1343a5612c62092c65b2af
JoseMerida97 <jose.merida@training.assuresoft.com>
2022-05-27 14:55:28 -0400

    FirstVersion

---
+++ src/Demo/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Demo;
+
+import Factory.UserFilesParser;
+
+
+/**
+ *
+ * @author JOSE MERIDA ROCA
+ */
+public class Main {
+    
+     public static void main(String[] args){
+         UserFilesParser parser=new UserFilesParser("xml");
+         parser.getUsers("Jose");
+
+    }
+    
+    
+   
+    
+}
